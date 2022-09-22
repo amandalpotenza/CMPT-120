0e204f94f99d4aaf9526999117f3a5e9b91ee7b5
amandalpotenza <112631726+amandalpotenza@users.noreply.github.com>
2022-09-22 09:43:58 -0400

    HW 2.5

--- Assignment 2.5/double_int_list_practice.py
+++ Assignment 2.5/double_int_list_practice.py
@@ -1,29 +1,39 @@
 def main():
   
   #set this to any double
-  doubleValue =
+  doubleValue = 1.2
   
   #set this to any int
-  intValue = 
+  intValue = 1
   
   #print out addition, subtraction, multiplication, and division of these two values
-  
-  
+  print(doubleValue + intValue)
+  print(doubleValue - intValue)
+  print(doubleValue * intValue)
+  print(doubleValue / intValue)
   
   #populate this list
-  myFriends = []
+  myFriends = ["Madeleine", "Nora", "Lidija", "Carmela"]
   
   #print out your friends at index 2 and index 3
-  
+  print(myFriends[2])
+  print(myFriends[3])
   
   #populate this list with five numbers
-  fiveNumbers = []
+  fiveNumbers = [1,6,3,7,8]
   
   #do each of the four equations with different numbers each time.
-  
+  print(fiveNumbers[0] + fiveNumbers[1])
+  print(fiveNumbers[3] - fiveNumbers[2])
+  print(fiveNumbers[4] * fiveNumbers[1])
+  print(fiveNumbers[1] / fiveNumbers[0])
+
   #now replace two of the numbers in the list with a different number (using name of list[x] = ?, not rewriting the fiveNumber list)
-  
+  fiveNumbers[0] = 2
+  fiveNumbers[3] = 5
   #print out the list
+
+  print(fiveNumbers)
   
   
 main()
--- Assignment 2.5/if_else_sign_practice.py
+++ Assignment 2.5/if_else_sign_practice.py
@@ -5,10 +5,17 @@ def main():
         If they're 18 and older but younger than 21, tell them they can only buy nicotine products in some states
         If they're less than 18, they can only purchase candy cigarettes and sody pops
     '''
-    
-  
 
-  
-  
+    age = int(input("type age here:"))
+    if age >= 25:
+        print("you can buy alcohol, nicotine products, and rent a car")
+    elif age >= 21:
+        print("you can buy alcohol and nicotine products, but you cannot rent a car")
+    elif age >= 18:
+        print("you can buy nicotine in some states")
+    else:
+        print("sorry! candy cigarettes and sody pops only")
+
+
   
 main()
