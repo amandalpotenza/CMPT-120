d997d892f26bb4baaabd48d09640377fc6d58548
amandalpotenza <112631726+amandalpotenza@users.noreply.github.com>
2022-10-31 08:40:53 -0400

    Assignment 5

--- Assignment 5/Debug.py
+++ Assignment 5/Debug.py
@@ -8,14 +8,14 @@ def main():
         
     #can you google and find what function you should use to check if it's numeric (an int?)?
     intInput = input("enter an int")
-    if intInput.?:
+    if intInput.isdigit():
         print("int!")
     else:
         print("not int :(")
     
     #what about if it's both letters and numbers?
     alphIntInput = input("Enter letters and numbers")
-    if alphIntInput.?:
+    if alphIntInput.isalnum():
         print("Letters and numbers!")
     else:
         print("weird characters :(")
@@ -28,11 +28,26 @@ def main():
         print("not asterisk :(")
         
     #now write code to check if the input was either an asterisk OR an ampersand (&)
-    
+    astOrAmb = input("Enter an asterisk or ambersand please")
+    if astOrAmb == "*" or astOrAmb == "&":
+        print("good!")
+    else:
+        print("not asterisk or ampersand :(")
         
     #do the live example we did in class: ask user to input an integer, but before you cast it to an int, check that it's an integer before doing your variable = int(variable) command
-    
+    num = input("enter an int")
+    if num.isdigit():
+        num = int(num)
+        print(num)
+    else:
+        print("no")
+
     # last challenge: find out how to check if the string input has the substring "marist"
     #google this one ;) substring is the key google term
-    
+    checkMarist = input("Check to see if Marist in code")
+    school = "Marist"
+    if school in checkMarist:
+        print("Marist is in string!")
+    else:
+        print("no")
 main()
