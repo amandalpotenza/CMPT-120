6b1ba9bd72f890e3fcf47016377288c8d094384c
amandalpotenza <112631726+amandalpotenza@users.noreply.github.com>
2022-11-13 18:06:10 -0500

    Assignment 6

--- Assignment 6/Debug.py
+++ Assignment 6/Debug.py
@@ -12,6 +12,9 @@ class Employee:
 class Cake:
     #can you fill out the rest of this for me? im dumb
     #the cake needs to have the cake flavor and cake frosting stored
+    def __init__(self, flavor, frosting):
+        self.flavor = flavor
+        self.frosting = frosting
 
 class Cat:
     def __init__(self, name, age, fur_length):
@@ -33,35 +36,51 @@ class Car:
         self.color = color
         
     #create your own function! what do you want it to do?
-    
+    #i want to define tShirt attributes!
+class tShirt:
+    def __int__(self, size, color, sleeve_length):
+        self.size = size
+        self.color = color
+        self.sleeve_length = sleeve_length
    
 def main():
     #fill this one out with a dog's name and age.. can be your dog, friend's dog, etc
-    newDog = Dog(? , ?)
+    dog1 = Dog("George" , 1)
     print(dog1.name, dog1.age)
     
     #and what about a new employee
-    newEmployee =
+    newEmployee = Employee("Bob", 12321, "Finance")
     #how would we print out each of the variables from newEmployee?
-    print()
-    
+    print(newEmployee.name)
+    print(newEmployee.idNumber)
+    print(newEmployee.department)
+
     #now create and print out a cake you make
-    
+    cake1 = Cake("Vanilla", "Chocolate")
+    print(cake1.flavor)
+    print(cake1.frosting)
     
     
     #and now create another cake and print it out
-    
+    cake2 = Cake("Chocolate", "Cream Cheese")
+    print(cake2.flavor)
+    print(cake2.frosting)
     
     
     #create a cat!
-    cat1 = Cat()
+    cat1 = Cat("Olivia", 10, "long")
     #create another cat!
+    cat2 = Cat("Meridith", 8, "short")
     
     #What would we put here to print out the result of breedGuess for cat1?
-    print(cat1.?)
+    print(cat1.breedGuess())
     
     #create a car!
+    car1 = Car("Jeep", 2020, "White")
     
     #Now print out the function you made for car :)
+    print(car1.model)
+    print(car1.year)
+    print(car1.color)
 
 main()
--- Assignment 6/student.py
+++ Assignment 6/student.py
@@ -25,14 +25,44 @@ create a function because this college is a wacky one- every day they generate a
 '''
 
 class student:
+    def __init__(self, name, student_id, year, major, gpa):
+        self.name = name
+        self.student_id = student_id
+        self.year = year
+        self.major = major
+        self.gpa = gpa
+
+    def honors(self):
+        if self.gpa > 3.5:
+            return True
+        else:
+            return False
+
+    def food_raffle(self):
+        import random
+        rand_id = random.randrange(1111,9999)
+        if self.student_id == rand_id:
+            return("winner! student (name) gets free lunch!")
+        else:
+            return("Looooser!")
     
-    
-    
-    
-    
-    
+
 def main():
     #create three students and check if they get free lunch and if they qualify for honors
-    
+    student1 = student("Amanda", 4567, "sophomore", "computer science", 4.0)
+    student2 = student("Liz", 2983, "sophomore", "psychology", 3.2)
+    student3 = student("Maddy", 3498, "sophomore", "cybersecurity", 3.5)
+
+    #prints whether each student wins free lunch
+    print("Did you win the food raffle?")
+    print(student1.name, str(student1.food_raffle()))
+    print(student2.name, str(student2.food_raffle()))
+    print(student3.name, str(student3.food_raffle()))
+
+    #prints whether students 1,2, and 3 are eligible for honors
+    print("\nAre you eligible for honors?")
+    print(student1.name, str(student1.honors()))
+    print(student2.name, str(student2.honors()))
+    print(student3.name, str(student3.honors()))
     
 main()
